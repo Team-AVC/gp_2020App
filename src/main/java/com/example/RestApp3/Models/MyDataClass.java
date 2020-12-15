7e44d8d75e49f437e96804a080668bbd0a56efd0
Tharindu Deshananda <tdeshananda@gmail.com>
2020-12-15 17:50:01 +0530

    changed code

--- src/main/java/com/example/RestApp3/Models/MyDataClass.java
+++ src/main/java/com/example/RestApp3/Models/MyDataClass.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name = "SensorData")
+@Table(name = "sensordata")
 public class MyDataClass {
     @Id
     @GeneratedValue
--- src/main/java/com/example/RestApp3/Repos/MyRepo.java
+++ src/main/java/com/example/RestApp3/Repos/MyRepo.java
@@ -9,13 +9,13 @@ import java.util.List;
 
 public interface MyRepo extends JpaRepository<MyDataClass,Integer> {
 
-    @Query("from SensorData where dataValue<?1 or dataValue>?2")
+    @Query("from MyDataClass where dataValue<?1 or dataValue>?2")
     public List<MyDataClass>getOutOfRange(int lower,int higher);
 
-    @Query("from SensorData where dataValue>?1 and dataValue<?2")
+    @Query("from MyDataClass where dataValue>?1 and dataValue<?2")
     public List<MyDataClass>getInRange(int lower,int higher);
 
-    @Query("from SensorData where sensorId=?1 order by date")
+    @Query("from MyDataClass where sensorId=?1 order by date")
     public List<MyDataClass>getSignalsOfSensor(int id);
 
 }
