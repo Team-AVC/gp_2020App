19a77bd323108e6f38b23632c36581e1beb09153
RavinduDon <gunarath_se17015@stu.kln.ac.lk>
2020-12-30 22:37:16 +0530

    Exception handling

--- src/main/java/com/example/RestApp3/Service/IMPL/AlertGenerater.java
+++ src/main/java/com/example/RestApp3/Service/IMPL/AlertGenerater.java
@@ -26,23 +26,27 @@ public class AlertGenerater implements AlertGeneratorRepo {
     private long preSigId=0;
     @Scheduled(cron = "*/5 * * * * *")
     public void notifyTemp(){
-        sensorDatas=dataRepo.findAll();
-        data=sensorDatas.get(sensorDatas.size()-1);
-        sensor=data.getSensor();
-        sigId=data.getSigId();
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-        if(sigId!=preSigId) {
-            if (data.getDataValue() > sensor.getThreshold()) {
-                Alert alert=new Alert(data.getSensorId(), sensor, sensor.getSensorName() + " reading value is too high",data.getTimestamp());
-                alertRepo.save(alert);
-                System.out.println(sensor.getSensorName() +" reading value is too high");
-                System.out.println("Warning Email/SMS sent");
-            }else{
-                System.out.println(sensor.getSensorName() +" reading value is normal");
-                Alert alert=new Alert(data.getSensorId(), sensor,sensor.getSensorName() + " reading value is normal",data.getTimestamp());
-                alertRepo.save(alert);
+        try {
+            sensorDatas = dataRepo.findAll();
+            data = sensorDatas.get(sensorDatas.size() - 1);
+            sensor = data.getSensor();
+            sigId = data.getSigId();
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            if (sigId != preSigId) {
+                if (data.getDataValue() > sensor.getThreshold()) {
+                    Alert alert = new Alert(data.getSensorId(), sensor, sensor.getSensorName() + " reading value is too high", data.getTimestamp());
+                    alertRepo.save(alert);
+                    System.out.println(sensor.getSensorName() + " reading value is too high");
+                    System.out.println("Warning Email/SMS sent");
+                } else {
+                    System.out.println(sensor.getSensorName() + " reading value is normal");
+                    Alert alert = new Alert(data.getSensorId(), sensor, sensor.getSensorName() + " reading value is normal", data.getTimestamp());
+                    alertRepo.save(alert);
+                }
+                preSigId = sigId;
             }
-            preSigId=sigId;
+        }catch(IndexOutOfBoundsException e){
+            System.out.println("Database is empty , Please insert data to the database");
         }
     }
 }
